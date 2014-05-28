b72df60a0a457293b4d41846d43cf56dd666c111
Arona Jones <warlordjones@gmail.com>
2014-05-28 19:55:58 +0100

    Version number didn't bump...

--- src/main/java/enderglove/common/lib/LibInfo.java
+++ src/main/java/enderglove/common/lib/LibInfo.java
@@ -10,15 +10,15 @@
 package enderglove.common.lib;
 
 /**
- * @author Surseance (Johnny Eatmon) 
+ * @author Surseance (Johnny Eatmon)
  * Email: surseance@autistici.org
- * 
+ *
  */
 public class LibInfo
 {
 	public static final String ID = "EnderGlove";
 	public static final String NAME = "EnderGlove";
-	public static final String VERSION = "1.2.1"; // Major, minor, revision
+	public static final String VERSION = "1.3.0"; // Major, minor, revision
 
 	public static final String CLIENT_PROXY = "enderglove.client.ClientProxy";
 	public static final String COMMON_PROXY = "enderglove.common.CommonProxy";
