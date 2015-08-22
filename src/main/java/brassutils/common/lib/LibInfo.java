aa7336e5d1c0f6cbb6f47ce8732af413d4df53bc
Arona Jones <warlordjones@gmail.com>
2015-08-22 12:30:18 +0100

    Update version

--- src/main/java/brassutils/common/lib/LibInfo.java
+++ src/main/java/brassutils/common/lib/LibInfo.java
@@ -17,7 +17,7 @@ public class LibInfo
 	public static final String ID = "brassutils";
 	public static final String NAME = "BrassUtils";
 	public static final String NAME_SPACED = "Brass Utils";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	public static final String CLIENT_PROXY = "brassutils.client.ClientProxy";
 	public static final String COMMON_PROXY = "brassutils.common.CommonProxy";
